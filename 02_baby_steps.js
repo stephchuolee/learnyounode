20db047a59ea226d49f91c1696daa1535b2ca6c0
schlorantics <schuolee@gmail.com>
2015-08-31 15:03:37 +0000

    Complete exercise 2: baby steps

--- 02_baby_steps.js
+++ 02_baby_steps.js
@@ -1,1 +1,19 @@
-console.log(process.argv)
+function sum(array){
+  var length = array.length
+  var total = 0 
+
+  for (i=2; i<length; i++){
+    total += +array[i];
+  }
+  return total
+}
+
+console.log(sum(process.argv))
+
+
+// Official Answer: 
+// var result = 0 
+// for (var i=2; i<process.argv.length; i++){
+//   result += Number(process.argv[i])
+// }
+// console.log(result)
